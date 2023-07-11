523d5776d1dacccf144f25dadec5f867e6081f6b
Jaabir-Ahamed <post2jaabir@gmail.com>
2023-07-10 23:26:56 -0700

    Version 1 of the Driver License Generator, have the class setup and working

--- driverLicense.java
+++ driverLicense.java
@@ -1,14 +1,27 @@
-public class driverLicense {
+class driverLicense {
     private String name;
     private String address;
-    private double height;
-    private double weight;
+    private int height;
+    private int weight;
     private String hairColor;
     private String eyeColor;
-    private String licenseNumber;
+    private String driverLicenseNumber;
     private static int licenseCount = 1000;
 
-    // Copy constructor
+    private static int instanceCount = 0;
+
+    //constructor
+    public driverLicense(String name, String address, int height, int weight, String hairColor, String eyeColor){
+        //instantiate variables
+        this.name=name;
+        this.address=address;
+        this.height=height;
+        this.weight=weight;
+        this.hairColor=hairColor;
+        this.eyeColor=eyeColor;
+    }
+
+    //constructor copy
     public driverLicense(driverLicense other) {
         this.name = other.name;
         this.address = other.address;
@@ -16,85 +29,22 @@ public class driverLicense {
         this.weight = other.weight;
         this.hairColor = other.hairColor;
         this.eyeColor = other.eyeColor;
-        this.licenseNumber = other.licenseNumber;
     }
 
-    // Overloaded constructors
+    //overloaded constructor that counts instance
+    public driverLicense(int token){
+        token = instanceCount++;
+    }
+
+    //overloaded constructor that calls numberGenerator()
     public driverLicense() {
-        countInstance();
-        licenseNumber = numberGenerator();
+        numberGenerator();
     }
 
-    public driverLicense(String name, String address, double height, double weight, String hairColor, String eyeColor) {
-        this.name = name;
-        this.address = address;
-        this.height = height;
-        this.weight = weight;
-        this.hairColor = hairColor;
-        this.eyeColor = eyeColor;
-        countInstance();
-        licenseNumber = numberGenerator();
-    }
-
-    private void countInstance() {
+    //Number generator method
+    int numberGenerator() {
         licenseCount++;
-    }
-
-    private String numberGenerator() {
-        String stateAbbreviation = "CA"; // Example state abbreviation
-        return stateAbbreviation + licenseCount;
-    }
-
-    // Accessor and mutator methods
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public String getHairColor() {
-        return hairColor;
-    }
-
-    public void setHairColor(String hairColor) {
-        this.hairColor = hairColor;
-    }
-
-    public String getEyeColor() {
-        return eyeColor;
-    }
-
-    public void setEyeColor(String eyeColor) {
-        this.eyeColor = eyeColor;
-    }
-
-    public String getLicenseNumber() {
-        return licenseNumber;
+        driverLicenseNumber = "CA" + licenseCount;
+        return licenseCount;
     }
 }
--- main.java
+++ main.java
@@ -1,18 +1,142 @@
 public class main {
+    static class driverLicense {
+        private String name;
+        private String address;
+        private int height;
+        private int weight;
+        private String hairColor;
+        private String eyeColor;
+        private String driverLicenseNumber;
+        private static int licenseCount = 1000;
+
+        private static int instanceCount = 0;
+
+        //accessor methods
+        public String getName() {
+            return name;
+        }
+        public String getAddress() {
+            return address;
+        }
+        public int getHeight() {
+            return height;
+        }
+        public int getWeight() {
+            return weight;
+        }
+        public String getHairColor() {
+            return hairColor;
+        }
+        public String getEyeColor() {
+            return eyeColor;
+        }
+        public String getDriverLicenseNumber() {
+            return driverLicenseNumber;
+        }
+        public static int getLicenseCount() {
+            return licenseCount;
+        }
+        public static int getInstanceCount() {
+            return instanceCount;
+        }
+
+        //mutator methods
+        public void setName(String name) {
+            this.name = name;
+        }
+        public void setAddress(String address) {
+            this.address = address;
+        }
+        public void setHeight(int height) {
+            this.height = height;
+        }
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+        public void setHairColor(String hairColor) {
+            this.hairColor = hairColor;
+        }
+        public void setEyeColor(String eyeColor) {
+            this.eyeColor = eyeColor;
+        }
+        public void setDriverLicenseNumber(String driverLicenseNumber) {
+            this.driverLicenseNumber = driverLicenseNumber;
+        }
+        public static void setLicenseCount(int licenseCount) {
+            driverLicense.licenseCount = licenseCount;
+        }
+        public static void setInstanceCount(int instanceCount) {
+            driverLicense.instanceCount = instanceCount;
+        }
+
+
+
+        //constructor
+        public driverLicense(String name, String address, int height, int weight, String hairColor, String eyeColor){
+            //instantiate variables
+            this.name=name;
+            this.address=address;
+            this.height=height;
+            this.weight=weight;
+            this.hairColor=hairColor;
+            this.eyeColor=eyeColor;
+        }
+
+        //constructor copy
+        public driverLicense(driverLicense other) {
+            this.name = other.name;
+            this.address = other.address;
+            this.height = other.height;
+            this.weight = other.weight;
+            this.hairColor = other.hairColor;
+            this.eyeColor = other.eyeColor;
+        }
+
+        //overloaded constructor that counts instance
+        public driverLicense(int token){
+            token = instanceCount++;
+        }
+
+        //overloaded constructor that calls numberGenerator()
+        public driverLicense() {
+            numberGenerator();
+        }
+
+        //Number generator method
+        int numberGenerator() {
+            licenseCount++;
+            driverLicenseNumber = "CA" + licenseCount;
+            return licenseCount;
+        }
+    }
+
+    // Helper method to print driverLicense details
+    public static void printDriverLicenseDetails(driverLicense license) {
+        System.out.println("Name: " + license.getName());
+        System.out.println("Address: " + license.getAddress());
+        System.out.println("Height: " + license.getHeight());
+        System.out.println("Weight: " + license.getWeight());
+        System.out.println("Hair Color: " + license.getHairColor());
+        System.out.println("Eye Color: " + license.getEyeColor());
+        System.out.println("Driver License Number: " + license.getDriverLicenseNumber());
+    }
     public static void main(String[] args) {
-        // Create objects of the driverLicense class using overloaded constructors
-        driverLicense license1 = new driverLicense();
-        driverLicense license2 = new driverLicense("John Doe", "123 Main St", 180, 75, "Brown", "Blue");
-        driverLicense license3 = new driverLicense("Jane Smith", "456 Elm St", 165, 60, "Blonde", "Green");
 
-        // Call accessor and mutator methods for testing purposes
-        license1.setName("Alice");
-        System.out.println(license1.getName());
-        System.out.println(license2.getWeight());
-        license3.setHairColor("Black");
-        System.out.println(license3.getHairColor());
+        // Create driverLicense objects using different constructors
+        driverLicense license1 = new driverLicense("John Doe", "123 Main St", 180, 75, "Brown", "Blue");
+        driverLicense license2 = new driverLicense(1234);
+        driverLicense license3 = new driverLicense();
 
-        // Create a copy of one of the objects using the copy constructor
-        driverLicense licenseCopy = new driverLicense(license2);
+        // Output the details of each driverLicense object
+        System.out.println("Driver License 1:");
+        printDriverLicenseDetails(license1);
+        System.out.println();
+
+        System.out.println("Driver License 2:");
+        printDriverLicenseDetails(license2);
+        System.out.println();
+
+        System.out.println("Driver License 3:");
+        printDriverLicenseDetails(license3);
     }
 }
